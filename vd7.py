feacc3834d6efea9f7386234cf3cc0a5675cd7b6
pvkhai-hh <phanvankhai18102005@gmail.com>
2025-08-28 23:57:32 +0700

    vankhai

---
+++ vd7.py
@@ -0,0 +1,5 @@
+def add(a, b):
+ result = a + b
+ return result
+import math_libs
+math_libs.add(4,5)
---
+++ vd8.py
@@ -0,0 +1,17 @@
+# module1.py
+question = 'What is the meaning of Life, the Universe,
+and Everything?
+answer = 42
+# module2.py
+question = 'What is your quest?'
+answer = 'To seek the holy grail.'
+import module1
+import module2
+print(module1.question)
+print(module2.question)
+print(module1.answer)
+print(module2.answer)
+What is the meaning of Life, the Universe, and Everything?
+What is your quest?
+42
+To seek the holy grail.
