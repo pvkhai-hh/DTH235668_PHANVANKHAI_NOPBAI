51d2ce5b104a12cd053979533a767cdfc3c71e8b
Phan Văn Khải <phanvankhai18102005@gmail.com>
2025-09-01 17:27:13 +0700

    vankhai

---
+++ Chương 2/Câu3.py
@@ -0,0 +1,6 @@
+toan=float(input("Nhập điểm Toán: "))
+ly=float(input("Nhập điểm Lý: "))
+hoa=float(input("Nhập điểm Hóa: "))
+dtb=(toan+ly+hoa)/3
+print("Điểm trung bình= ",dtb)
+print("Điểm làm tròn= ",round(dtb,2))
